9e9f757666c30aaf96eb87d3bb535519d7ed3864
J <J@afshans-MacBook-Pro.local>
2020-10-15 01:35:05 -0700

    Updates

---
+++ precache-manifest.df71d95ce476ce9002c80a109ed36006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f4c279bbadd71c76623d75b1f7bb8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661eabde85cd4cbc8e8b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc7f598b0ac3874ffb82",
+    "url": "/static/js/2.3929b1dc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3929b1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661eabde85cd4cbc8e8b",
+    "url": "/static/js/main.cc460dbc.chunk.js"
+  },
+  {
+    "revision": "148ed7df2f6dd10b473d",
+    "url": "/static/js/runtime-main.38166e12.js"
+  }
+]);
